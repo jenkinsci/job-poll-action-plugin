6d5f20df70ed4ddb279e6eb78d21195ffe4ea90f
Daniel Beland <dcendents@gmail.com>
2018-09-18 13:20:43 -0400

    adding support for pipeline projects
    support jenkins instances with a context iin the url
    resolve the job url instead of assuming its path (to fix errors when a job is inside a folder)

--- src/main/java/org/jenkins/ci/plugins/JobPollTransientProjectActionFactory.java
+++ src/main/java/org/jenkins/ci/plugins/JobPollTransientProjectActionFactory.java
@@ -1,18 +1,18 @@
 /*
  * The MIT License
- * 
+ *
  * Copyright (c) 2012, Jesse Farinacci
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,8 +26,8 @@ package org.jenkins.ci.plugins;
 
 import hudson.Extension;
 import hudson.model.Action;
-import hudson.model.TransientProjectActionFactory;
-import hudson.model.AbstractProject;
+import hudson.model.Job;
+import jenkins.model.TransientActionFactory;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -36,16 +36,21 @@ import java.util.Collections;
  * A very simple {@link hudson.model.TransientProjectActionFactory} which
  * creates new {@link JobPollAction}s for the target
  * {@link hudson.model.AbstractProject}.
- * 
+ *
  * @author <a href="mailto:jieryn@gmail.com">Jesse Farinacci</a>
  * @since 1.0
  */
 @Extension
 public final class JobPollTransientProjectActionFactory extends
-        TransientProjectActionFactory {
+TransientActionFactory<Job> {
     @Override
     public Collection<? extends Action> createFor(
-            @SuppressWarnings("rawtypes") final AbstractProject target) {
+            @SuppressWarnings("rawtypes") final Job target) {
         return Collections.singleton(new JobPollAction(target));
     }
+
+	@Override
+	public Class<Job> type() {
+		return Job.class;
+	}
 }
